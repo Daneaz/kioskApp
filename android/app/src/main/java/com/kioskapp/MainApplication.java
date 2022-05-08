7672ec7e5f94b37e118bb0e14ee861fa1a5fa580
Eugene <eugenewwj@gmail.com>
2022-05-08 23:36:49 +0800

    support video play on homepage

--- android/app/src/main/java/com/kioskapp/MainApplication.java
+++ android/app/src/main/java/com/kioskapp/MainApplication.java
@@ -11,6 +11,7 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.microsoft.codepush.react.CodePush;
+import com.brentvatne.react.ReactVideoPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -28,6 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
 
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          packages.add( new ReactVideoPackage());
+
           return packages;
         }
 
